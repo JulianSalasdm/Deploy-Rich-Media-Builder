164ac96b4db99477e2abf836c8ada05caf1e54c9
Juli Salas <juliansalasdm@gmail.com>
2026-02-09 12:32:57 -0300

    Fix 2.0 para compatibilidad con Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: false,
+      },
+    },
   },
   server: {
     port: 3000,
